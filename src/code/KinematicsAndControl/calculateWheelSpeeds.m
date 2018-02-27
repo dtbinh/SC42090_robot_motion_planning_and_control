04d36f5317169d1f72c53984b282821e2d67fd67
Xander Gerrmann <xander@gerrmann.nl>
2018-02-27 20:16:56 +0100

    Cleanup to original.

--- src/code/KinematicsAndControl/calculateWheelSpeeds.m
+++ src/code/KinematicsAndControl/calculateWheelSpeeds.m
@@ -1,10 +1,9 @@
 function [ LeftWheelVelocity, RightWheelVelocity ] = calculateWheelSpeeds( vu, omega, parameters )
 %CALCULATEWHEELSPEEDS This function computes the motor velocities for a differential driven robot
 
-wheelRadius   = parameters.wheelRadius;
+wheelRadius = parameters.wheelRadius;
 halfWheelbase = parameters.interWheelDistance/2;
 
-WheelVelocity      = (2/wheelRadius)/[1,1;1,-1]*[vu;halfWheelbase*omega];
-RightWheelVelocity = WheelVelocity(1);
-LeftWheelVelocity  = WheelVelocity(2);
+LeftWheelVelocity = TODO;
+RightWheelVelocity = TODO;
 end
